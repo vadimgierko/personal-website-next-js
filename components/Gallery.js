a51c281d2c20874e71ca83663454312fc0be40a2
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2022-10-22 23:51:36 +0200

    Add basic unstyled modal for displaying images from gallery

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import Modal from "react-bootstrap/Modal";
 
 export default function Gallery({ images }) {
+	const [show, setShow] = useState(false);
+	const [modalSrc, setModalSrc] = useState();
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	if (!images) return null;
 
 	const Img = ({ src }) => (
-		<img src={src} width="100%" className="mb-3" alt="" />
+		<img
+			src={src}
+			width="100%"
+			className="mb-3"
+			alt=""
+			onClick={() => {
+				setModalSrc(src);
+				handleShow();
+			}}
+		/>
 	);
 
 	return (
@@ -23,6 +40,13 @@ export default function Gallery({ images }) {
 						<Img key={key} src={images[key].default.src} />
 					))}
 			</Col>
+			<Modal show={show} onHide={handleClose} size="lg">
+				<Modal.Header closeButton></Modal.Header>
+				<Modal.Body>
+					<img src={modalSrc} alt="" width="100%" />
+				</Modal.Body>
+				{/* <Modal.Footer>Modal footer</Modal.Footer> */}
+			</Modal>
 		</Row>
 	);
 }

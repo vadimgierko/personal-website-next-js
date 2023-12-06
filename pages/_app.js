d6853836cda642751074455ed2c72041dfd4bd22
Vadim Gierko <85487884+vadimgierko@users.noreply.github.com>
2023-12-06 12:27:26 +0000

    integrate google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import { useEffect } from "react";
 import { ThemeProvider } from "../contexts/useTheme";
 import Layout from "../layout";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 	// import bootstrap:
@@ -14,6 +15,19 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider>
 			<Layout>
+				{/**=============== Google tag (gtag.js) ====================*/}
+				<Script src="https://www.googletagmanager.com/gtag/js?id=G-SDYR1XY35B" strategy="afterInteractive" />
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-SDYR1XY35B');
+					`}
+				</Script>
+				{/**=============== Google tag (gtag.js) END ================*/}
+
 				<Component {...pageProps} />
 			</Layout>
 		</ThemeProvider>

530f4a8b756ca79a963153c03e70eafb397fff92
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2025-01-27 23:58:09 +0100

    do not fetch data from github

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -74,36 +74,36 @@ export async function getStaticProps({ params }) {
 	let pageContent = await getPageContent(params.slug);
 
 	//======================================== for dev projects:
-	if (pageContent.pageType === "devProject") {
-		// FETCH REPO DATA FROM GITHUB ONLY IF PROJECT IS PUBLIC:
-		if (pageContent.public) {
-			const repoData = await getRepoDataFromGitHub(pageContent.repoName);
-			const readmeMarkdown = await getRepoReadmeFileContentFromGitHub(
-				repoData.name
-			);
-			// replace readme's h1 with h2:
-			const fixedMarkdown = readmeMarkdown.replace("#", "##");
-			pageContent = {
-				...pageContent,
-				content: fixedMarkdown,
-				description: repoData.description,
-				createdAt: repoData.created_at,
-				updatedAt: repoData.updated_at,
-				externalLinks: [
-					{
-						icon: "github",
-						link: "https://github.com/vadimgierko/" + repoData.name,
-						description: "Zobacz kod na GitHub",
-					},
-					{
-						icon: "global",
-						link: repoData.homepage,
-						description: "Strona www projektu",
-					},
-				],
-			};
-		}
-	}
+	// if (pageContent.pageType === "devProject") {
+	// 	// FETCH REPO DATA FROM GITHUB ONLY IF PROJECT IS PUBLIC:
+	// 	if (pageContent.public) {
+	// 		const repoData = await getRepoDataFromGitHub(pageContent.repoName);
+	// 		const readmeMarkdown = await getRepoReadmeFileContentFromGitHub(
+	// 			repoData.name
+	// 		);
+	// 		// replace readme's h1 with h2:
+	// 		const fixedMarkdown = readmeMarkdown.replace("#", "##");
+	// 		pageContent = {
+	// 			...pageContent,
+	// 			content: fixedMarkdown,
+	// 			description: repoData.description,
+	// 			createdAt: repoData.created_at,
+	// 			updatedAt: repoData.updated_at,
+	// 			externalLinks: [
+	// 				{
+	// 					icon: "github",
+	// 					link: "https://github.com/vadimgierko/" + repoData.name,
+	// 					description: "Zobacz kod na GitHub",
+	// 				},
+	// 				{
+	// 					icon: "global",
+	// 					link: repoData.homepage,
+	// 					description: "Strona www projektu",
+	// 				},
+	// 			],
+	// 		};
+	// 	}
+	// }
 	return {
 		props: {
 			pageContent,
